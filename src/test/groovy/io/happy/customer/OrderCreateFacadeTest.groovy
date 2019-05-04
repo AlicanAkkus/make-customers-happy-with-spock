60cdfdc03f1910220839a0480fd3c5c0d745234f
Alican Akkus <alican.akkus94@gmail.com>
2019-05-04 22:52:33 +0300

    adds interaction based order create test

--- src/test/groovy/io/happy/customer/OrderCreateFacadeTest.groovy
+++ src/test/groovy/io/happy/customer/OrderCreateFacadeTest.groovy
@@ -28,4 +28,27 @@ class OrderCreateFacadeTest extends Specification {
             it.orderCreatedDate.toLocalDate()
         }
     }
+
+    void "should create order and verify interactions"() {
+        given:
+        def userId = 998
+        def paymentMethod = "CARD"
+        def price = 100
+
+        and:
+        1 * orderCommissionCalculator.calculate("CARD", 100) >> 2.3
+        0 * _._
+
+        when:
+        def order = orderCreateFacade.create(userId, paymentMethod, price)
+
+        then:
+        with(order) {
+            it.userId == 998
+            it.paymentMethod == "CARD"
+            it.price == 100
+            it.commissionAmount == 2.3
+            it.orderCreatedDate.toLocalDate()
+        }
+    }
 }
