7ba46ee61cf6d89ce92c26a477acfbcbc5e5ff9c
Alican Akkus <alican.akkus94@gmail.com>
2019-05-07 14:11:19 +0300

    adds new test includes that keywoard

--- src/test/groovy/io/happy/customer/OrderCreateFacadeTest.groovy
+++ src/test/groovy/io/happy/customer/OrderCreateFacadeTest.groovy
@@ -2,6 +2,8 @@ package io.happy.customer
 
 import spock.lang.Specification
 
+import static org.spockframework.util.Assert.that
+
 class OrderCreateFacadeTest extends Specification {
 
     def orderCommissionCalculator = Mock(OrderCommissionCalculator)
@@ -50,6 +52,31 @@ class OrderCreateFacadeTest extends Specification {
             it.commissionAmount == 2.3
             it.orderCreatedDate.toLocalDate()
         }
+    }
+
+    void "should create order and verify interactions with that keywoard"() {
+        given:
+        def userId = 998
+        def paymentMethod = "CARD"
+        def price = 100
+
+        and:
+        1 * orderCommissionCalculator.calculate("CARD", 100) >> 2.3
+        0 * _._
+
+        when:
+        def order = orderCreateFacade.create(userId, paymentMethod, price)
+
+        then:
+        that orderShouldCreated(order)
+    }
+
+    boolean orderShouldCreated(Order order) {
+        order.userId == 998
+        order.paymentMethod == "CARD"
+        order.price == 100
+        order.commissionAmount == 2.3
+        order.orderCreatedDate.toLocalDate()
     }
 
     void "should not create order when price is zero"() {
